44948bb28ddb0558b851fa7814fd418c4c2128bc
Andrzej Więckowski <andywiecko@gmail.com>
2022-03-24 19:18:05 +0100

    [Feature] Edge Constraint Triangulation

--- Tests/TriangulatorEditorTests.cs
+++ Tests/TriangulatorEditorTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using System;
 using System.Linq;
 using Unity.Collections;
 using Unity.Mathematics;
@@ -7,7 +8,7 @@ namespace andywiecko.BurstTriangulator.Editor.Tests
 {
     public static class TriangulatorTestExtensions
     {
-        public static (int, int, int)[] ToTrisTuple(this NativeArray<int>.ReadOnly triangles) => Enumerable
+        public static (int, int, int)[] ToTrisTuple(this NativeList<int> triangles) => Enumerable
             .Range(0, triangles.Length / 3)
             .Select(i => (triangles[3 * i], triangles[3 * i + 1], triangles[3 * i + 2]))
             .ToArray();
@@ -15,8 +16,8 @@ namespace andywiecko.BurstTriangulator.Editor.Tests
 
     public class TriangulatorEditorTests
     {
-        private (int, int, int)[] Triangles => triangulator.Triangles.ToTrisTuple();
-        private float2[] Positions => triangulator.Positions.ToArray();
+        private (int, int, int)[] Triangles => triangulator.Output.Triangles.ToTrisTuple();
+        private float2[] Positions => triangulator.Output.Positions.ToArray();
 
         private Triangulator triangulator;
 
@@ -46,10 +47,10 @@ namespace andywiecko.BurstTriangulator.Editor.Tests
             using var positions = new NativeArray<float2>(managedPositions, Allocator.Persistent);
 
             triangulator.Settings.RefineMesh = false;
+            var input = triangulator.Input;
+            input.Positions = positions;
 
-            triangulator
-                .Schedule(positions.AsReadOnly(), default)
-                .Complete();
+            triangulator.Run();
 
             ///  3 ------- 2
             ///  |      . `|
@@ -58,6 +59,62 @@ namespace andywiecko.BurstTriangulator.Editor.Tests
             ///  0 ------- 1
 
             Assert.That(Triangles, Is.EqualTo(new[] { (1, 0, 2), (2, 0, 3) }));
+        }
+
+        private static readonly TestCaseData[] validateInputPositionsTestData = new[]
+        {
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(0, 1)
+                }
+            ) { TestName = "Test Case 1 (points count less than 3)" },
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(0, 0),
+                    math.float2(1, 1),
+                    math.float2(0, 1)
+                }
+            ) { TestName = "Test Case 2 (duplicated position)" },
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, float.NaN),
+                    math.float2(1, 1),
+                    math.float2(0, 1)
+                }
+            ) { TestName = "Test Case 3 (point with NaN)" },
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, float.PositiveInfinity),
+                    math.float2(1, 1),
+                    math.float2(0, 1)
+                }
+            ) { TestName = "Test Case 4 (point with +inf)" },
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, float.NegativeInfinity),
+                    math.float2(1, 1),
+                    math.float2(0, 1)
+                }
+            ) { TestName = "Test Case 4 (point with -inf)" },
+        };
+
+        [Test, TestCaseSource(nameof(validateInputPositionsTestData))]
+        public void ValidateInputPositionsTest(float2[] managedPositions)
+        {
+            using var positions = new NativeArray<float2>(managedPositions, Allocator.Persistent);
+            triangulator.Settings.RefineMesh = false;
+            triangulator.Input = new() { Positions = positions };
+            Assert.Throws<ArgumentException>(() => triangulator.Run());
         }
 
         [Test]
@@ -79,9 +136,10 @@ namespace andywiecko.BurstTriangulator.Editor.Tests
             settings.MinimumArea = 0.3f;
             settings.MaximumArea = 0.3f;
 
-            triangulator
-                .Schedule(positions.AsReadOnly(), default)
-                .Complete();
+            var input = triangulator.Input;
+            input.Positions = positions;
+
+            triangulator.Run();
 
             ///  3 ------- 2
             ///  |` .   . `|
@@ -104,6 +162,207 @@ namespace andywiecko.BurstTriangulator.Editor.Tests
                 (2, 1, 4), (1, 0, 4), (4, 0, 3), (3, 2, 4)
             };
             Assert.That(Triangles, Is.EqualTo(expectedTriangles));
+        }
+
+        private static readonly TestCaseData[] edgeConstraintsTestData = new[]
+        {
+            new TestCaseData(
+                //   6 ----- 5 ----- 4
+                //   |    .`   `.    |
+                //   | .`         `. |
+                //   7 ------------- 3
+                //   | `.         .` |
+                //   |    `.   .`    |
+                //   0 ----- 1 ----- 2
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, 0),
+                    math.float2(2, 0),
+                    math.float2(2, 1),
+                    math.float2(2, 2),
+                    math.float2(1, 2),
+                    math.float2(0, 2),
+                    math.float2(0, 1),
+                }, new[] { 1, 5 })
+            {
+                TestName = "Test case 1",
+                ExpectedResult = new[]
+                {
+                    (2, 1, 3),
+                    (4, 3, 5),
+                    (5, 3, 1),
+                    (1, 7, 5),
+                    (6, 5, 7),
+                    (1, 0, 7),
+                }
+            },
+
+            new TestCaseData(
+                //   6 ----- 5 ----- 4
+                //   |    .`   `.    |
+                //   | .`         `. |
+                //   7 ------------- 3
+                //   | `.         .` |
+                //   |    `.   .`    |
+                //   0 ----- 1 ----- 2
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, 0),
+                    math.float2(2, 0),
+                    math.float2(2, 1),
+                    math.float2(2, 2),
+                    math.float2(1, 2),
+                    math.float2(0, 2),
+                    math.float2(0, 1),
+                }, new[] { 1, 5, 1, 4 })
+            {
+                TestName = "Test case 2",
+                ExpectedResult = new[]
+                {
+                    (2, 1, 3),
+                    (4, 3, 1),
+                    (1, 5, 4),
+                    (1, 7, 5),
+                    (6, 5, 7),
+                    (1, 0, 7),
+                }
+            },
+
+            new TestCaseData(
+                //   9 ----- 8 ----- 7 ----- 6
+                //   |    .` `   . ``  `.    |
+                //   | .`  :   ..         `. |
+                //  10    :  ..        ..... 5
+                //   |   :` .   ....`````    |
+                //   | ,.:..`````            |
+                //  11 ..................... 4
+                //   | `. ` . .           .` |
+                //   |    `.    ` . .  .`    |
+                //   0 ----- 1 ----- 2 ----- 3
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, 0),
+                    math.float2(2, 0),
+                    math.float2(3, 0),
+                    math.float2(3, 1),
+                    math.float2(3, 2),
+                    math.float2(3, 3),
+                    math.float2(2, 3),
+                    math.float2(1, 3),
+                    math.float2(0, 3),
+                    math.float2(0, 2),
+                    math.float2(0, 1),
+                }, new[] { 3, 9, 8, 5 })
+            {
+                TestName = "Test case 3",
+                ExpectedResult = new[]
+                {
+                    (3, 2, 11),
+                    (6, 5, 7),
+                    (9, 8, 3),
+                    (3, 10, 9),
+                    (8, 7, 5),
+                    (8, 5, 4),
+                    (8, 4, 3),
+                    (3, 11, 10),
+                    (2, 1, 11),
+                    (1, 0, 11),
+                }
+            },
+        };
+
+        [Test, TestCaseSource(nameof(edgeConstraintsTestData))]
+        public (int, int, int)[] ConstraintDelaunayTriangulationTest(float2[] managedPositions, int[] constraints)
+        {
+            using var positions = new NativeArray<float2>(managedPositions, Allocator.Persistent);
+            using var constraintEdges = new NativeArray<int>(constraints, Allocator.Persistent);
+
+            var settings = triangulator.Settings;
+            settings.ConstrainEdges = true;
+            settings.RefineMesh = false;
+
+            var input = triangulator.Input;
+            input.Positions = positions;
+            input.ConstraintEdges = constraintEdges;
+
+            triangulator.Run();
+
+            return Triangles;
+        }
+
+        private static readonly TestCaseData[] validateConstraintDelaunayTriangulationTestData = new[]
+        {
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, 0),
+                    math.float2(1, 1),
+                    math.float2(0, 1),
+                },
+                new[]{ 0, 2, 1, 3 }
+            ) { TestName = "Test Case 1 (edge-edge intersection)" },
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, 0),
+                    math.float2(1, 1),
+                    math.float2(0, 1),
+                },
+                new[]{ 0, 2, 0, 2 }
+            ) { TestName = "Test Case 2 (duplicated edge)" },
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, 0),
+                    math.float2(1, 1),
+                    math.float2(0, 1),
+                },
+                new[]{ 0, 0 }
+            ) { TestName = "Test Case 3 (zero-length edge)" },
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(0.5f, 0),
+                    math.float2(1, 0),
+                    math.float2(1, 1),
+                    math.float2(0, 1),
+                },
+                new[]{ 0, 2 }
+            ) { TestName = "Test Case 4 (edge collinear with other point)" },
+            new TestCaseData(
+                new[]
+                {
+                    math.float2(0, 0),
+                    math.float2(1, 0),
+                    math.float2(1, 1),
+                    math.float2(0, 1),
+                },
+                new[]{ 0, 5, 1 }
+            ) { TestName = "Test Case 5 (odd number of elements in constraints buffer)" },
+        };
+
+        [Test, TestCaseSource(nameof(validateConstraintDelaunayTriangulationTestData))]
+        public void ValidateConstraintDelaunayTriangulationTest(float2[] managedPositions, int[] constraints)
+        {
+            using var positions = new NativeArray<float2>(managedPositions, Allocator.Persistent);
+            using var constraintEdges = new NativeArray<int>(constraints, Allocator.Persistent);
+
+            var settings = triangulator.Settings;
+            settings.ConstrainEdges = true;
+            settings.RefineMesh = false;
+
+            var input = triangulator.Input;
+            input.Positions = positions;
+            input.ConstraintEdges = constraintEdges;
+
+            Assert.Throws<ArgumentException>(() => triangulator.Run());
         }
     }
 }
