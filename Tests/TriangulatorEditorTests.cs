9726819937d09e7216864ed79a3ec2781a9f63a7
andywiecko <andywiecko@gmail.com>
2023-11-12 11:05:09 +0100

    test: extract test utils

--- Tests/TriangulatorEditorTests.cs
+++ Tests/TriangulatorEditorTests.cs
@@ -1,5 +1,4 @@
 using NUnit.Framework;
-using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 using Unity.Burst;
@@ -7,34 +6,9 @@ using Unity.Collections;
 using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine.TestTools;
-using UnityEngine.TestTools.Utils;
 
 namespace andywiecko.BurstTriangulator.Editor.Tests
 {
-    public static class TriangulatorTestExtensions
-    {
-        public static (int, int, int)[] GetTrisTuple(this Triangulator triangulator) =>
-            triangulator.Output.Triangles.ToTrisTuple();
-
-        private static (int, int, int)[] ToTrisTuple(this NativeList<int> triangles) => Enumerable
-            .Range(0, triangles.Length / 3)
-            .Select(i => (triangles[3 * i], triangles[3 * i + 1], triangles[3 * i + 2]))
-            .OrderBy(i => i.Item1).ThenBy(i => i.Item2).ThenBy(i => i.Item3)
-            .ToArray();
-    }
-
-    public class Float2Comparer : IEqualityComparer<float2>
-    {
-        private readonly float epsilon;
-        public static readonly Float2Comparer Instance = new(0.0001f);
-        public Float2Comparer(float epsilon) => this.epsilon = epsilon;
-        public static Float2Comparer With(float epsilon) => new(epsilon);
-        public bool Equals(float2 expected, float2 actual) =>
-            Utils.AreFloatsEqual(expected.x, actual.x, epsilon) &&
-            Utils.AreFloatsEqual(expected.y, actual.y, epsilon);
-        public int GetHashCode(float2 _) => 0;
-    }
-
     public class TriangulatorEditorTests
     {
         [Test]
@@ -1372,7 +1346,7 @@ namespace andywiecko.BurstTriangulator.Editor.Tests
             triangulator.Run();
             var localTriangles = triangulator.GetTrisTuple();
 
-            Assert.That(SortTrianglesIds(localTriangles), Is.EquivalentTo(SortTrianglesIds(nonLocalTriangles)));
+            Assert.That(TestUtils.SortTrianglesIds(localTriangles), Is.EquivalentTo(TestUtils.SortTrianglesIds(nonLocalTriangles)));
             Assert.That(localTriangles, Has.Length.EqualTo(24));
         }
 
@@ -1875,29 +1849,6 @@ namespace andywiecko.BurstTriangulator.Editor.Tests
             };
 
             triangulator.Run();
-        }
-
-        private static (int i, int j, int k)[] SortTrianglesIds((int i, int j, int k)[] triangles)
-        {
-            var copy = triangles.ToArray();
-            for (int i = 0; i < triangles.Length; i++)
-            {
-                var tri = triangles[i];
-                var t = math.int3(tri.i, tri.j, tri.k);
-                var min = math.cmin(t);
-                var id = -1;
-                for (int ti = 0; ti < 3; ti++)
-                {
-                    if (min == t[ti])
-                    {
-                        id = ti;
-                        break;
-                    }
-                }
-
-                copy[i] = (min, t[(id + 1) % 3], t[(id + 2) % 3]);
-            }
-            return copy;
         }
     }
 }
