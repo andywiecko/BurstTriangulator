029ecfd41aee64366adb3e6100bf38c3f02e3060
andywiecko <andywiecko@gmail.com>
2024-08-21 18:05:21 +0200

    fix: missing using

--- Runtime/Triangulator.cs
+++ Runtime/Triangulator.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
